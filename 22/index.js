4327d32f3b68fd59c3902d7ec241ff59d2e6f75a
syedtalhaaskari <syedtalhaaskari@gmail.com>
2024-03-25 01:47:37 +0500

    Complete 22

---
+++ 22/index.js
@@ -0,0 +1,4 @@
+var arr = ['1', '2', '3'];
+console.log(arr);
+arr.length = 2;
+console.log(arr[5].length);
---
+++ 22/index.ts
@@ -0,0 +1,7 @@
+const arr = ['1', '2', '3'];
+
+console.log(arr);
+
+arr.length = 2;
+
+console.log(arr[5].length);

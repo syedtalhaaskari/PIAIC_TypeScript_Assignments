0e83c17ee0e1a11cf64bcadfe23c1316f8ac12c1
syedtalhaaskari <syedtalhaaskari@gmail.com>
2024-03-25 00:12:47 +0500

    Complete 09

---
+++ 09/index.js
@@ -0,0 +1,2 @@
+var favorite_number = 5;
+console.log("My favorite number is ".concat(favorite_number));
---
+++ 09/index.ts
@@ -0,0 +1,3 @@
+const favorite_number = 5;
+
+console.log(`My favorite number is ${favorite_number}`);

201785b130e44c9b833a4b33e10572ae4ba246cd
syedtalhaaskari <syedtalhaaskari@gmail.com>
2024-03-25 00:12:35 +0500

    Complete 07

---
+++ 07/index.js
@@ -0,0 +1,8 @@
+console.log('Addition');
+console.log('1 + 7 = ' + (1 + 7));
+console.log('\nSubtraction');
+console.log('9 - 1 = ' + (9 - 1));
+console.log('\nMultiplication');
+console.log('2 * 4 = ' + (2 * 4));
+console.log('\nDivision');
+console.log('16 / 2 = ' + (16 / 2));

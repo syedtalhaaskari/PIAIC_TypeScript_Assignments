8aeb5cc74d4257ea95626de341b689f6c4cad6c1
syedtalhaaskari <syedtalhaaskari@gmail.com>
2024-03-25 01:47:21 +0500

    Complete 21

---
+++ 21/index.js
@@ -0,0 +1,4 @@
+var items = {};
+items['countries'] = ['Pakistan', 'UAE', 'Saudi Arabia'];
+items['languages'] = ['Urdu', 'Arabic', 'English'];
+console.log(items);
---
+++ 21/index.ts
@@ -0,0 +1,6 @@
+const items: Record<string, string[]> = {};
+
+items['countries'] = ['Pakistan', 'UAE', 'Saudi Arabia'];
+items['languages'] = ['Urdu', 'Arabic', 'English'];
+
+console.log(items);

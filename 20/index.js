cc65318f77dd8246fa06660ab14defe37a6f9ec0
syedtalhaaskari <syedtalhaaskari@gmail.com>
2024-03-25 01:47:15 +0500

    Complete 20

---
+++ 20/index.js
@@ -0,0 +1,7 @@
+var items = [];
+items[0] = 'mountains';
+items[1] = 'rivers';
+items[2] = 'countries';
+items[3] = 'cities';
+items[4] = 'languages';
+console.log(items);
---
+++ 20/index.ts
@@ -0,0 +1,9 @@
+const items: string[] = [];
+
+items[0] = 'mountains';
+items[1] = 'rivers';
+items[2] = 'countries';
+items[3] = 'cities';
+items[4] = 'languages';
+
+console.log(items);

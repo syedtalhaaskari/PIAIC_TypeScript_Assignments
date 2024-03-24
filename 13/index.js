0396c58b6f0986475749213c06c922a3c49d6df4
syedtalhaaskari <syedtalhaaskari@gmail.com>
2024-03-25 01:46:25 +0500

    Complete 13

---
+++ 13/index.js
@@ -0,0 +1,2 @@
+var items = ['Jannah', 'Country', 'Company'];
+items.forEach(function (item) { return console.log("I would like to own (a) ".concat(item)); });
---
+++ 13/index.ts
@@ -0,0 +1,3 @@
+const items = ['Jannah', 'Country', 'Company'];
+
+items.forEach(item => console.log(`I would like to own (a) ${item}`));

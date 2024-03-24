644bd8572a042c56c4700a78a5a0b1ee24f4ccea
syedtalhaaskari <syedtalhaaskari@gmail.com>
2024-03-25 01:47:05 +0500

    Complete 19

---
+++ 19/index.js
@@ -0,0 +1,4 @@
+// 14
+var guests = ['Prophet Muhammad PBUH', 'Prophet Musa AS', 'Prophet Sulayman AS'];
+guests.forEach(function (guest) { return console.log("I would like to invite ".concat(guest, " on dinner.")); });
+console.log("\nTotal number are of guests are ".concat(guests.length));
---
+++ 19/index.ts
@@ -0,0 +1,6 @@
+// 14
+const guests = ['Prophet Muhammad PBUH', 'Prophet Musa AS', 'Prophet Sulayman AS'];
+
+guests.forEach(guest => console.log(`I would like to invite ${guest} on dinner.`));
+
+console.log(`\nTotal number are of guests are ${guests.length}`);

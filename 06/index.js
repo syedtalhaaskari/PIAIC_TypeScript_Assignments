9e1d271e46492bc67fbb5ddb9721532c25309e38
syedtalhaaskari <syedtalhaaskari@gmail.com>
2024-03-25 00:12:29 +0500

    Complete 06

---
+++ 06/index.js
@@ -0,0 +1,5 @@
+var person_name = '\tSyed Talha Askari\n';
+console.log('Before Strip:');
+console.log("|".concat(person_name, "|"));
+console.log('After Strip:');
+console.log("|".concat(person_name.trim(), "|"));
---
+++ 06/index.ts
@@ -0,0 +1,6 @@
+const person_name = '\tSyed Talha Askari\n';
+
+console.log('Before Strip:');
+console.log(`|${person_name}|`);
+console.log('After Strip:');
+console.log(`|${person_name.trim()}|`);

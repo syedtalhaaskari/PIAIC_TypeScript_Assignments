80b591024cbde2295f96eb607e671957c0345568
syedtalhaaskari <syedtalhaaskari@gmail.com>
2024-03-25 01:47:54 +0500

    Complete 23

---
+++ 23/index.ts
@@ -0,0 +1,30 @@
+// @ts-nocheck
+console.log('1 == "1"? I predict True');
+console.log(1 == "1");
+
+console.log('\n1 === "1"? I predict False');
+console.log(1 === "1");
+
+console.log('\n[] == {}? I predict False');
+console.log([] == {});
+
+console.log('\n[] === {}? I predict False');
+console.log([] === {});
+
+console.log('\n[] === 0? I predict False');
+console.log([] === 0);
+
+console.log('\n{} == 0? I predict False');
+console.log({} == 0);
+
+console.log('\nfalse == 0? I predict true');
+console.log(false == 0);
+
+console.log('\nfalse === 0? I predict false');
+console.log(false === 0);
+
+console.log('\ntypeof [] === "object"? I predict true');
+console.log(typeof [] === "object");
+
+console.log('\ntypeof {} === "object"? I predict true');
+console.log("1" > 0);

77b46853b50cebac5da8dc6690a6b0629f81884b
syedtalhaaskari <syedtalhaaskari@gmail.com>
2024-03-25 20:42:59 +0500

    Complete 27

---
+++ 27/red.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let alien_color = 'red';
+if (alien_color === 'green') {
+    console.log('Green, You earned 5 points');
+}
+else if (alien_color === 'yellow') {
+    console.log('Yellow, You earned 10 points');
+}
+else if (alien_color === 'red') {
+    console.log('Yellow, You earned 15 points');
+}

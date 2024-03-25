a141bd6b2c1b967569049be763efba695fd1b395
syedtalhaaskari <syedtalhaaskari@gmail.com>
2024-03-26 00:25:37 +0500

    Complete 34

---
+++ 34/index.ts
@@ -0,0 +1,9 @@
+const pizzas = ['cheesy tikka', 'jalapeeno', 'creemy beef'];
+
+pizzas.forEach((pizza) => console.log(pizza));
+
+console.log();
+
+pizzas.forEach((pizza) => console.log(`I like ${pizza} pizza.`));
+
+console.log('\nI really love pizza!');

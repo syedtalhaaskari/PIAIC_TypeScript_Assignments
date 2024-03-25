355e60908e4fef317a55a0b69dedebfb4af1c041
syedtalhaaskari <syedtalhaaskari@gmail.com>
2024-03-26 00:26:08 +0500

    Complete 39

---
+++ 39/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const city_country = (city, country) => {
+    return `${city}, ${country}`;
+};
+console.log(city_country('Karachi', 'Pakistan'));
+console.log(city_country('Dubai', 'UAE'));
+console.log(city_country('Makkah', 'Saudi Arabia'));

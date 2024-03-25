a90826042da82b9b23570fb86e4108b1b156f0c4
syedtalhaaskari <syedtalhaaskari@gmail.com>
2024-03-26 00:25:26 +0500

    Complete 32

---
+++ 32/index.ts
@@ -0,0 +1,10 @@
+const current_users = ['talha', 'rockyBhai', 'aijazSahab', 'papaKiPari', 'username'];
+const new_users = ['rockyBHAI', '', 'userNAME', 'newUser', 'ahmed', 'ROCkyBhai'];
+
+new_users.forEach((new_user) => {
+    if (current_users.map((current_user) => current_user.toLowerCase()).includes(new_user.toLowerCase())) {
+        console.log(`${new_user} is already taken. Please enter a new username.`);
+    } else {
+        console.log(`${new_user} is available.`);
+    }
+})

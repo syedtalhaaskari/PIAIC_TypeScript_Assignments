21b7e60c2999f2ed50c8ddc6ed17d849cddf0c56
syedtalhaaskari <syedtalhaaskari@gmail.com>
2024-03-26 00:25:31 +0500

    Complete 33

---
+++ 33/index.ts
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+numbers.forEach((num) => {
+    if (num === 1) {
+        console.log(`${num}st`);
+    } else if (num === 2) {
+        console.log(`${num}nd`);
+    } else if (num === 3) {
+        console.log(`${num}rd`);
+    } else {
+        console.log(`${num}th`);
+    }
+})
